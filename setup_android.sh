479f258c3a491a26e4b8aafc2ac964aed4d3e766
TheDarkBug <adrianoliviero23@gmail.com>
2022-02-11 14:31:37 +0100

    Updated to latest android sdk (dx was missing, i added it to this repo)

--- setup_android.sh
+++ setup_android.sh
@@ -8,8 +8,8 @@ if [ ! -d android-sdk ]
 then
 	sleep 1
 	mkdir -v android-sdk
-	[ ! -f commandlinetools-linux-7583922_latest.zip ] && wget "https://dl.google.com/android/repository/commandlinetools-linux-7583922_latest.zip"
-	unzip commandlinetools-linux-7583922_latest.zip
+	[ ! -f commandlinetools-linux-8092744_latest.zip ] && wget "https://dl.google.com/android/repository/commandlinetools-linux-8092744_latest.zip"
+	unzip commandlinetools-linux-8092744_latest.zip
 	mv cmdline-tools android-sdk/tools
 	printf "\nInstalling sdk...\n"
 	sleep 1
@@ -17,14 +17,10 @@ then
 	export ANDROID_SDK=$(pwd)
 	cd tools/bin
 	./sdkmanager --update --sdk_root=$ANDROID_SDK
-	./sdkmanager --install "build-tools;30.0.3" --sdk_root=$ANDROID_SDK
+	./sdkmanager --install "build-tools;32.0.0" --sdk_root=$ANDROID_SDK
 	./sdkmanager --install platform-tools --sdk_root=$ANDROID_SDK
-	./sdkmanager --install "platforms;android-30" --sdk_root=$ANDROID_SDK
+	./sdkmanager --install "platforms;android-32" --sdk_root=$ANDROID_SDK
 	unset ANDROID_SDK
-	cd $START_DIR
-else
-	cd android-sdk
-	export ANDROID_SDK=$(pwd)
 	cd $START_DIR
 fi
 
@@ -32,19 +28,11 @@ printf "\nInstalling ndk...\n"
 if [ ! -d android-ndk ]
 then
 	sleep 1
-	[ ! -f android-ndk-r22b-linux-x86_64.zip ] && wget "https://dl.google.com/android/repository/android-ndk-r22b-linux-x86_64.zip"
-	unzip android-ndk-r22b-linux-x86_64.zip
-	mv android-ndk-r22b android-ndk
-	cd android-ndk
-	export ANDROID_NDK=$(pwd)
-	cd build/tools
-	python make_standalone_toolchain.py --arch arm --api 30 --install-dir=android_toolchain_ARM_API30
-	mv android_toolchain_ARM_API30 $START_DIR
-	export ANDROID_TOOLCHAIN=$START_DIR/android_toolchain_ARM_API30
-	cd $START_DIR
-else
-	cd android-ndk
-	export ANDROID_NDK=$(pwd)
+	[ ! -f android-ndk-r23b-linux.zip ] && wget "https://dl.google.com/android/repository/android-ndk-r23b-linux.zip"
+	unzip android-ndk-r23b-linux.zip
+	mv android-ndk-r23b android-ndk
+	cp dx android-sdk/build-tools/32.0.0/dx
+	cp dx.jar android-sdk/build-tools/32.0.0/lib/dx.jar
 	cd $START_DIR
 fi
 
