73f247f27ff79775356728f9f9759bf58bbbba86
TheDarkBug <adrianoliviero23@gmail.com>
2022-02-27 20:04:02 +0100

    Moved to sdk 28 for better retro compatibility

--- setup_android.sh
+++ setup_android.sh
@@ -2,6 +2,7 @@
 set -e
 
 export START_DIR=$(pwd)
+export SDK_REL_NUM=28
 
 printf "Installing android sdkmanager...\n"
 if [ ! -d android-sdk ]
@@ -17,9 +18,9 @@ then
 	export ANDROID_SDK=$(pwd)
 	cd tools/bin
 	./sdkmanager --update --sdk_root=$ANDROID_SDK
-	./sdkmanager --install "build-tools;32.0.0" --sdk_root=$ANDROID_SDK
+	./sdkmanager --install "build-tools;$SDK_REL_NUM.0.0" --sdk_root=$ANDROID_SDK
 	./sdkmanager --install platform-tools --sdk_root=$ANDROID_SDK
-	./sdkmanager --install "platforms;android-32" --sdk_root=$ANDROID_SDK
+	./sdkmanager --install "platforms;android-$SDK_REL_NUM" --sdk_root=$ANDROID_SDK
 	unset ANDROID_SDK
 	cd $START_DIR
 fi
@@ -31,8 +32,8 @@ then
 	[ ! -f android-ndk-r23b-linux.zip ] && wget "https://dl.google.com/android/repository/android-ndk-r23b-linux.zip"
 	unzip android-ndk-r23b-linux.zip
 	mv android-ndk-r23b android-ndk
-	cp dx android-sdk/build-tools/32.0.0/dx
-	cp dx.jar android-sdk/build-tools/32.0.0/lib/dx.jar
+	cp dx android-sdk/build-tools/$SDK_REL_NUM.0.0/dx
+	cp dx.jar android-sdk/build-tools/$SDK_REL_NUM.0.0/lib/dx.jar
 	cd $START_DIR
 fi
 
